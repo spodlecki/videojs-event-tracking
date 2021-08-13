65f0cc8164bd4faf1a8112a1ff87e4f3aebe4747
spodlecki <spodlecki@skintertainment.com>
2021-08-12 21:12:01 -0500

    test suite

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -4,24 +4,11 @@ module.exports = function(config) {
     usePhantomJS: false
   };
 
-  // On Travis CI, we can only run in Firefox and Chrome; so, enforce that.
-  if (process.env.TRAVIS) {
-    config.browsers = ['Firefox', 'travisChrome'];
-  }
-
-  // If no browsers are specified, we enable `karma-detect-browsers`
-  // this will detect all browsers that are available for testing
-  if (!config.browsers.length) {
-    detectBrowsers.enabled = true;
-  }
-
   config.set({
     basePath: '..',
-    frameworks: ['qunit', 'detectBrowsers'],
+    frameworks: ['qunit'],
     files: [
       'node_modules/video.js/dist/video-js.css',
-
-
       'node_modules/sinon/pkg/sinon.js',
       'node_modules/video.js/dist/video.js',
       'test/dist/bundle.js'
@@ -32,7 +19,7 @@ module.exports = function(config) {
         flags: ['--no-sandbox']
       }
     },
-    detectBrowsers: detectBrowsers,
+    browsers: ['Firefox', 'travisChrome'],
     reporters: ['dots'],
     port: 9876,
     colors: true,
